baa974cf738ee56f41793808f64b5fe02b3d1916
Your Name <nemanja.lazovic@dev.telnet.rs>
2019-03-11 13:48:22 +0100

    changed to MediaType.APPLICATION_JSON

--- src/main/java/com/telusko/demoREST/authorController.java
+++ src/main/java/com/telusko/demoREST/authorController.java
@@ -1,7 +1,5 @@
 package com.telusko.demoREST;
 
-import java.lang.reflect.Array;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
@@ -17,65 +15,50 @@ import javax.ws.rs.core.MediaType;
 import pkg.Author;
 import DAO.AuthorDAO;
 
-
 @Path("/authors")
 public class authorController {
-	
+
 	AuthorDAO dao = new AuthorDAO();
-	
 
 	@GET
 	@Path("/test")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMessage() {
-        return "pleasure is mine!";
-    }
-    
-    
-    	    
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMessage() {
+		return "pleasure is mine!";
+	}
+
 	@GET
-    @Produces(MediaType.APPLICATION_XML)
-    public ArrayList<Author>  getAllAuthors() {
-        return dao.getAllAuthors();
-    }
-	
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Author> getAllAuthors() {
+		return dao.getAllAuthors();
+	}
+
 	@GET
 	@Path("/{authorID}")
-    @Produces(MediaType.APPLICATION_XML)
-    public Author  getAuthor(@PathParam("authorID") int id) {
-        return dao.getAuthor(id);
-    }
-	
-	
-	
-	
+	@Produces(MediaType.APPLICATION_JSON)
+	public Author getAuthor(@PathParam("authorID") int id) {
+		return dao.getAuthor(id);
+	}
+
 	@POST
-	@Consumes(MediaType.APPLICATION_XML)
-	public boolean addAuthor(Author author){
+	@Consumes(MediaType.APPLICATION_JSON)
+	public boolean addAuthor(Author author) {
 		return dao.insertAuthor(author);
 	}
-	
-	
-	
+
 	@PUT
-	@Consumes(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/{authorID}")
-	public boolean updateAuthor(@PathParam("authorID") int id, Author author){
+	public boolean updateAuthor(@PathParam("authorID") int id, Author author) {
 		author.setId(id);
 		return dao.updateAuthor(author);
 	}
-    
-	
+
 	@DELETE
-	@Consumes(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/{authorID}")
-	public boolean deleteAuthor(@PathParam("authorID") int id){
+	public boolean deleteAuthor(@PathParam("authorID") int id) {
 		return dao.deleteAuthor(id);
 	}
-    
-    
-
-	
-	
 
 }
