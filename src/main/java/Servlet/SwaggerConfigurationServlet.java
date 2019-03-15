4abab5fc81b364718fa7ff47010cbd63e349d69a
Your Name <nemanja.lazovic@dev.telnet.rs>
2019-03-15 14:24:56 +0100

    added new class SwaggerConfigurationServlet

---
+++ src/main/java/Servlet/SwaggerConfigurationServlet.java
@@ -0,0 +1,28 @@
+ package Servlet;
+
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+
+public class SwaggerConfigurationServlet extends HttpServlet {
+	
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		BeanConfig beanConfig=new BeanConfig();
+		beanConfig.setBasePath("/demoREST/webapi/");
+		beanConfig.setHost("localhost:8080");
+		beanConfig.setTitle("Authors/Books app Swagger Docs");
+		beanConfig.setResourcePackage("com.telusko.demoREST");
+		beanConfig.setPrettyPrint(true);
+		beanConfig.setScan(true);
+		beanConfig.setSchemes(new String[] {"http"});
+		beanConfig.setVersion("1.0");
+	}
+
+}
